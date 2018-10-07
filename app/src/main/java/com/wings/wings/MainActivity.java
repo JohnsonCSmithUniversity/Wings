e137d29822fcafbfc3f5032f6f4729020e1e4592
Jesusfi <izquierdo832@gmail.com>
2018-10-07 08:48:12 -0700

    all request work

--- app/src/main/java/com/wings/wings/MainActivity.java
+++ app/src/main/java/com/wings/wings/MainActivity.java
@@ -12,6 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -41,16 +44,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
                 makeRequestForUser("John@jcsu.edu");
                 makeRequestForReservation("URJXUL");
-                //startActivity(new Intent(MainActivity.this,BarcodeActivity.class));
-                //makeRequestForAirportInfo("LAX");
             }
         });
 
---
+++ app/src/main/java/com/wings/wings/UserInfoInstance.java
@@ -0,0 +1,4 @@
+package com.wings.wings;
+
+public class UserInfoInstance {
+}
